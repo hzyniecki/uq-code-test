8aebad5848f1bad344d6f2626c8e383af2a8efb7
hzyniecki <hzyniecki@gmail.com>
2024-02-01 11:00:22 -0600

    installed gh-pages

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
   devtool: false,
   output: {
     path: paths.build,
-    publicPath: '/',
+    publicPath: '/uq-code-test/',
     filename: '[name].[contenthash].bundle.js',
   },
   plugins: [
